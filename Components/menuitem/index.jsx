14b5438fdede0491012711fd56bf58dc6d0bbffa
erosboysuper <erosboysuper@gmail.com>
2021-09-06 11:58:16 -0700

    scss

--- Components/menuitem/index.jsx
+++ Components/menuitem/index.jsx
@@ -1,10 +1,14 @@
 import "./styles.scss";
-const MenuItem = ({ item: { title, image, id, linkUrl } }) => {
-  console.log(title, "text");
+const MenuItem = ({ item: { title, imageUrl, id, size, linkUrl } }) => {
+  console.log(title, imageUrl, "text");
   return (
-    <div className="container">
+    <div className={`${size} container`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="backgroundimage"
+      />
       <div className="content">
-        <h5 className="title">{title}</h5>
+        <h5 className="title">{title.toUpperCase()}</h5>
         <p className="shopnow">SHOP NOW</p>
       </div>
     </div>
